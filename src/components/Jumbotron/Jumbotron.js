d4a0f33e80ce1f503f5d9543734d104bc2637154
steliosk9 <steliosk9@gmail.com>
2019-08-30 11:11:33 -0500

    Start with create-react-app

---
+++ src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Jumbotron.css";
+
+
+const Jumbotron = () => (
+<div className="jumbotron jumbotron-fluid">
+  <div className="container text-center">
+    <h1 className="display-4">React Clicky Game!</h1>
+    <p className="lead">Click on any image to earn points, but dont do it twice!</p>
+  </div>
+</div>
+);
+
+export default Jumbotron;
